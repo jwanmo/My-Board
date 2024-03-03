31070e5c3cff7235beba2cb1f6bdc6d8caa184d0
jwanmo <joanmohamad.jm@gmail.com>
2024-03-03 01:15:26 +0100

    add new fields to register user in backend

--- modules/register.mjs
+++ modules/register.mjs
@@ -4,11 +4,11 @@ import SuperLogger from "./SuperLogger.mjs";
 import { insert } from "./db.mjs";
 
 
-export const registerUser = async (name, email, pswHash, city) => {
+export const registerUser = async (name, email, pswHash, street, city, zipcode, country) => {
     try {
 
-        const insertQuery = 'INSERT INTO "user" (name, email, pswhash, city) VALUES ($1, $2, $3, $4) RETURNING *';
-        const values = [name, email,  pswHash, city];
+        const insertQuery = 'INSERT INTO "user" (name, email, pswhash, street, city, zipcode, country) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+        const values = [name, email, pswHash ,street, city, zipcode, country];
 
         const insertRes = insert(insertQuery, values);
         
