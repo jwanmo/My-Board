700f70383e531be13068d28405205c597bf708da
jwanmo <joanmohamad.jm@gmail.com>
2024-03-03 01:15:53 +0100

    add new task api and login end point

--- server.mjs
+++ server.mjs
@@ -1,7 +1,9 @@
 import express from 'express' // Express is installed using npm
 import USER_API from './routes/usersRoute.mjs'; // This is where we have defined the API for working with users.
-
+import 'dotenv/config';
+import TASK_API from './routes/tasksRoute.mjs'; 
 import SuperLogger from './modules/SuperLogger.mjs';
+
 // Creating an instance of the server
 const server = express();
 // Selecting a port for the server to use.
@@ -18,6 +20,12 @@ server.use(express.static('public'));
 
 // Telling the server to use the USER_API (all urls that uses this code will have to have the /user after the base address)
 server.use("/user", USER_API);
+server.use("/task", TASK_API);
+
+server.post("/login",(req, res, next) => {
+    console.log("user " + req.params.username + "wnts to login!");
+    res.status(200).send().end();
+});
 
 // A get request handler example)
 server.get("/", (req, res, next) => {
